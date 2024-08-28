96f33982b5543826697e09952111e0e4c4a4dd9b
dov118 <contact@dov118.dev>
2024-08-28 17:51:12 +0200

    Related Feature v2

--- tests/LiveServices.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RawEsoStatus } from '@eso-status/types';
-import { LiveServices } from '../src';
-
-test('getData()', async (): Promise<void> => {
-  const data: RawEsoStatus[] = await LiveServices.getData();
-  expect(data.length !== 0).toEqual(true);
-});
