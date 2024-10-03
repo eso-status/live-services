8d3bc67fd2969d400a3808b4cca270d969c2272c
dov118 <contact@dov118.dev>
2024-10-03 17:19:30 +0200

    Related Feature v2

--- src/type/remoteRaw.type.ts
+++ src/type/remoteRaw.type.ts
@@ -2,17 +2,17 @@
  * Different raw data from announcements
  */
 export type RemoteRaw =
-  | '"The Elder Scrolls Online (EU)":"UP"'
-  | '"The Elder Scrolls Online (EU)":"DOWN"'
-  | '"The Elder Scrolls Online (NA)":"UP"'
-  | '"The Elder Scrolls Online (NA)":"DOWN"'
-  | '"The Elder Scrolls Online (PS4 - EU)":"UP"'
-  | '"The Elder Scrolls Online (PS4 - EU)":"DOWN"'
-  | '"The Elder Scrolls Online (PS4 - US)":"UP"'
-  | '"The Elder Scrolls Online (PS4 - US)":"DOWN"'
-  | '"The Elder Scrolls Online (XBox - EU)":"UP"'
-  | '"The Elder Scrolls Online (XBox - EU)":"DOWN"'
-  | '"The Elder Scrolls Online (XBox - US)":"UP"'
-  | '"The Elder Scrolls Online (XBox - US)":"DOWN"'
-  | '"The Elder Scrolls Online (PTS)":"UP"'
-  | '"The Elder Scrolls Online (PTS)":"DOWN"';
+  | 'The Elder Scrolls Online (EU):UP'
+  | 'The Elder Scrolls Online (EU):DOWN'
+  | 'The Elder Scrolls Online (NA):UP'
+  | 'The Elder Scrolls Online (NA):DOWN'
+  | 'The Elder Scrolls Online (PS4 - EU):UP'
+  | 'The Elder Scrolls Online (PS4 - EU):DOWN'
+  | 'The Elder Scrolls Online (PS4 - US):UP'
+  | 'The Elder Scrolls Online (PS4 - US):DOWN'
+  | 'The Elder Scrolls Online (XBox - EU):UP'
+  | 'The Elder Scrolls Online (XBox - EU):DOWN'
+  | 'The Elder Scrolls Online (XBox - US):UP'
+  | 'The Elder Scrolls Online (XBox - US):DOWN'
+  | 'The Elder Scrolls Online (PTS):UP'
+  | 'The Elder Scrolls Online (PTS):DOWN';
